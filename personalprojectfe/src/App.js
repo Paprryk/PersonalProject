e8599592b056cab4405279d247b665650965ab88
unknown <patryk040600@gmail.com>
2024-05-01 00:43:22 +0100

    PF Added basic dark theme toggle

--- personalprojectfe/src/App.js
+++ personalprojectfe/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/MainFrame/Header';
 import Introduction from './components/MainFrame/Intro';
 import TechnicalSkills from './components/MainFrame/TechnicalSkills';
@@ -7,11 +7,18 @@ import Projects from './components/MainFrame/Projects';
 import Education from './components/MainFrame/Education';
 import References from './components/MainFrame/References';
 import Navbar from './components/MainFrame/Navbar';
+import './App.css'; // Import global CSS file for app-wide styles
 
 const App = () => {
+  const [darkTheme, setDarkTheme] = useState(true); // Initial theme is dark
+
+  const toggleTheme = () => {
+    setDarkTheme(!darkTheme); // Toggle between dark and light theme
+  }
+
   return (
-    <div>
-      <Navbar />
+    <div className={darkTheme ? "dark-theme" : "light-theme"}>
+      <Navbar darkTheme={darkTheme} toggleTheme={toggleTheme} />
       <Header />
       <Introduction />
       <TechnicalSkills />
--- personalprojectfe/src/components/MainFrame/Navbar.jsx
+++ personalprojectfe/src/components/MainFrame/Navbar.jsx
@@ -1,18 +1,30 @@
+// Navbar.js
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import { Form } from 'react-bootstrap';
 
-const Navbar = () => {
-    return (
-        <nav>
-            <ul>
-                <li><a href="#introduction">Introduction</a></li>
-                <li><a href="#technical-skills">Technical Skills</a></li>
-                <li><a href="#work-history">Work History</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#education">Education</a></li>
-                <li><a href="#references">References</a></li>
-            </ul>
-        </nav>
-    );
+const Navbar = ({ darkTheme, toggleTheme }) => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#introduction">Introduction</a></li>
+        <li><a href="#technical-skills">Technical Skills</a></li>
+        <li><a href="#work-history">Work History</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#education">Education</a></li>
+        <li><a href="#references">References</a></li>
+        <li>
+          <Form.Check 
+            type="switch"
+            id="themeSwitch"
+            label={darkTheme ? 'Dark Theme' : 'Light Theme'}
+            checked={darkTheme}
+            onChange={toggleTheme}
+          />
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
